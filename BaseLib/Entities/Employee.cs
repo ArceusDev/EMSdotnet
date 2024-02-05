0252ca586ce29bf461bcb81ff08b3ee97c431f02
Hamza Afzal <95589034+ArceusDev@users.noreply.github.com>
2024-02-05 23:44:54 +0500

    EmployeeEntity created

---
+++ BaseLib/Entities/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseLib.Entities
+{
+    public class Employee : BaseEntity
+    {
+        public string? CivilId { get; set; }
+        public string? FileNumber { get; set; }
+        public string? FullName { get; set; }
+        public string? JobName { get; set; }
+        public string? Address { get; set; }
+        public string? TelephoneNumber { get; set; }
+        public string? Photo { get; set; }
+        public string? Other { get; set; }
+    }
+}
---
+++ BaseLib/Entities/Relationship.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace BaseLib.Entities
+{
+    public class Relationship
+    {
+        [JsonIgnore]
+        public List<Employee>? Employees { get; set; }
+    }
+}
